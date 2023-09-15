ddcdb032c7131d4ecc87c6345574e2a50012327d
swing <ziijchen@outlook.com>
2023-09-16 07:27:03 +0800

    new lc198

---
+++ solutions/lc198.c
@@ -0,0 +1,19 @@
+/**
+ * 198 medium, house-robber
+ * dp
+*/
+
+int rob(int* nums, int numsSize){
+    // dp0: max money when NOT stealing current house
+    int dp0 = 0;
+    // dp1: max money when stealing current house
+    int dp1 = nums[0];
+
+    for (int i = 1; i < numsSize; i++) {
+        int dp0new = fmax(dp0, dp1);
+        int dp1new = dp0 + nums[i];
+        dp0 = dp0new;
+        dp1 = dp1new;
+    }
+    return fmax(dp0, dp1);
+}
